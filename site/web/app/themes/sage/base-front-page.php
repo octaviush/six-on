e15889cf57a7e0a53e524de017b887c64b7a4dd9
octaviush <tremellh8@gmail.com>
2016-07-29 03:31:46 -0400

    server side

--- site/web/app/themes/sage/base-front-page.php
+++ site/web/app/themes/sage/base-front-page.php
@@ -20,7 +20,7 @@ use Roots\Sage\Wrapper;
     ?>
     <div class="wrap container top-space-home" role="document">
       <div class="content row">
-        <main class="col-sm-8 col-md-8">
+        <main class="main">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
         <?php if (Setup\display_sidebar()) : ?>
--- site/web/app/themes/sage/templates/content-video.php
+++ site/web/app/themes/sage/templates/content-video.php
@@ -9,3 +9,4 @@
 </article>
 
 </div>
+
